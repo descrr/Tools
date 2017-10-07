a75b835063fea7167d6d8207c0eb40143f34fe54
descrr <descrr@gmail.com>
2017-10-07 23:39:02 +0300

    Direction strategy has implemented

--- Strategy/Strategy/DirectionStrategy.cs
+++ Strategy/Strategy/DirectionStrategy.cs
@@ -6,7 +6,91 @@ using System.Threading.Tasks;
 
 namespace Strategy
 {
-	class DirectionStrategy
+	public class DirectionStrategySelector
 	{
+		const int MaxCycle = 1023;
+
+		private Dictionary<int, bool> XoResults;
+		public DirectionStrategySelector(Dictionary<int, bool> xoResults)
+		{
+			XoResults = xoResults;
+		}
+
+		public DirectionStrategy GetBestStrategy()
+		{
+			int profitCount = -1;
+			DirectionStrategy bestStrategy = null; 
+			for(int i = 0; i <= 1023; i++)
+			{
+				string strategyTemplate = Convert.ToString(i, 2);
+
+				var directionStrategy = new DirectionStrategy();
+				directionStrategy.InitStrategy(strategyTemplate);
+
+				bool forecastedDirection;
+				bool prevRealDirection = XoResults[0];
+				for (int j = 1; j < XoResults.Count; j++)
+				{
+					forecastedDirection = directionStrategy.GetNextDirection(prevRealDirection);
+					prevRealDirection = XoResults[j];
+				}
+
+				if(profitCount < directionStrategy.ProfitCounter)
+				{
+					profitCount = directionStrategy.ProfitCounter;
+					bestStrategy = directionStrategy;
+				}
+			}
+			return bestStrategy;
+		}
+	}
+
+	public class DirectionStrategy
+	{
+		private string StrategyTemplate;
+		private int TemplateIndex;
+
+		public bool? ForecastedDirection;
+		public Dictionary<int, bool> DirectionResults = new Dictionary<int, bool>();
+		public int ProfitCounter;
+		
+		public void InitStrategy(string strategyTemplate)
+		{
+			StrategyTemplate = strategyTemplate;
+			ForecastedDirection = null;
+			TemplateIndex = -1;
+			ProfitCounter = 0;
+			
+		}
+
+		public bool GetNextDirection(bool prevMarketDirection)
+		{
+			//renew profit counter
+			if(prevMarketDirection != null 
+			&& ForecastedDirection != null
+			&& prevMarketDirection == ForecastedDirection)
+			{
+				if (prevMarketDirection == ForecastedDirection)
+					++ProfitCounter;
+			}
+
+			//forecast next direction
+			++TemplateIndex;
+			if (TemplateIndex > StrategyTemplate.Length - 1)
+				TemplateIndex = 0;
+
+			//follow the trend
+			if (StrategyTemplate[TemplateIndex] == '1')
+			{
+				ForecastedDirection = prevMarketDirection;
+			}
+			else //reverse trend direction
+			{
+				ForecastedDirection = !prevMarketDirection;
+			}
+			DirectionResults[DirectionResults.Count] = (bool)ForecastedDirection;
+
+			return (bool)ForecastedDirection;
+		}
 	}
 }
--- Strategy/Strategy/Program.cs
+++ Strategy/Strategy/Program.cs
@@ -14,9 +14,15 @@ namespace Strategy
 			var history = dataLoader.LoadFromFile(@"C:\Projects\Tools\Strategy\Data\EURUSD5.csv");
 			var converter = new Bar2XoConverter();
 			var xoList = converter.Convert(history.Bars);
-			
-			var selector = new BetStrategySelector(xoList);
-			var strategy = selector.GetBestStrategy();
+
+			//xoList[xoList.Count] = false;
+
+			var directionStrategySelector = new DirectionStrategySelector(xoList);
+			var directionStrategy = directionStrategySelector.GetBestStrategy();
+			var betSelector = new BetStrategySelector(directionStrategy.DirectionResults);
+			var betStrategy = betSelector.GetBestStrategy();
+
+			Console.WriteLine("{0} ==> Next forecast: {1}, bet: {2}", history.Bars[history.Bars.Count - 1].Dt, directionStrategy.ForecastedDirection, betStrategy.CurrentBetInUnits);
 		}
 	}
 }
