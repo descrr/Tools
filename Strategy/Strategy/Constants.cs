11393d1942b5a7af1e5d2d50ec168567484cb8c4
Viachaslau Hurski <Viachaslau_Hurski@epam.com>
2017-11-17 15:34:34 +0300

    Rank has been increased

--- Strategy/Strategy/Constants.cs
+++ Strategy/Strategy/Constants.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Strategy
+﻿namespace Strategy
 {
 	public static class Constants
 	{
 		public static int MinRank = 9;
-		public static int MaxRank = 14;
+		public static int MaxRank = 16;
 		public static int MaxXoCount = 120;
 	}
 }
